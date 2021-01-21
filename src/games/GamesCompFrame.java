4d644ae8d837e575b4daef1bd27fc66867866bd6
ishaqadhel <ishaq.adhel@gmail.com>
2021-01-22 02:58:13 +0700

    update code v computer

--- src/games/GamesCompFrame.java
+++ src/games/GamesCompFrame.java
@@ -72,148 +72,149 @@ public class GamesCompFrame extends JFrame {
 		comp.setRandomName();
 		
 		gameOn = 1;
-		// Title Bar
-				setTitle("Tic Tac Toe");
-				setIconImage(Toolkit.getDefaultToolkit().getImage(Home.class.getResource("/image/icon.png")));
-						
-				// Frame Setting
-				setResizable(false);
-				setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				setBounds(100, 100, 705, 525);
-				contentPane = new JPanel();
-				contentPane.setBackground(new Color(47, 79, 79));
-				contentPane.setOpaque(false);
-				contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-				setContentPane(contentPane);
-				contentPane.setLayout(null);
-				
-				// Board
-				for(int i = 0; i < 9; i++)
-				{
-					btn[i] = new JButton("");
-					btn[i].setText(null);
-					btn[i].setBackground(new Color(224, 90, 71));
-					contentPane.add(btn[i]);
-					btn[i].addActionListener(new ActionListener() {
-						public void actionPerformed(ActionEvent arg0) {
-							JButton buttonClicked = (JButton) arg0.getSource();
-							btnActionPerformed(buttonClicked);
-						}
-					});
-				}
-				btn[0].setBounds(92, 86, 85, 85);
-				btn[1].setBounds(204, 86, 85, 85);
-				btn[2].setBounds(318, 86, 85, 85);
-				btn[3].setBounds(92, 189, 85, 85);
-				btn[4].setBounds(204, 189, 85, 85);
-				btn[5].setBounds(318, 189, 85, 85);
-				btn[6].setBounds(92, 292, 85, 85);
-				btn[7].setBounds(204, 292, 85, 85);
-				btn[8].setBounds(318, 292, 85, 85);
-				
-				// Play Again Button
-				JButton btnPlayAgain = new JButton("Play Again");
-				btnPlayAgain.setForeground(Color.WHITE);
-				btnPlayAgain.setFont(new Font("Tahoma", Font.BOLD, 15));
-				btnPlayAgain.setBackground(new Color(224, 90, 71));
-				btnPlayAgain.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-						JFrame frame = new JFrame("warning");
-						
-						if(JOptionPane.showConfirmDialog(frame, "Are you sure want to restart game?", "Tic Tac Toe", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_NO_OPTION)
-						{
-							playAgain();
-						}
-					}
-				});
-				btnPlayAgain.setBounds(176, 410, 131, 34);
-				contentPane.add(btnPlayAgain);
-				
-				// Exit Button
-				JButton btnExit = new JButton("Exit");
-				btnExit.setForeground(Color.WHITE);
-				btnExit.setFont(new Font("Tahoma", Font.BOLD, 15));
-				btnExit.setBackground(new Color(224, 90, 71));
-				btnExit.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-						JFrame frame = new JFrame("exit");
-						// Background
-						
-						if(JOptionPane.showConfirmDialog(frame, "Are you sure want to exit game?", "Tic Tac Toe", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_NO_OPTION)
-						{
-							
-							System.exit(0);
-						}
-					}
-				});
-				btnExit.setBounds(336, 410, 131, 34);
-				contentPane.add(btnExit);
-				
-				// Scoreboard X
-					// Scoreboard X Box
-				JPanel ScoreboardX = new JPanel();
-				ScoreboardX.setForeground(Color.WHITE);
-				ScoreboardX.setBackground(new Color(224, 90, 71));
-				ScoreboardX.setOpaque(true);
-				ScoreboardX.setBounds(445, 86, 150, 137);
-				contentPane.add(ScoreboardX);
-				ScoreboardX.setLayout(null);
 
-					// Scoreboard X Name
-				JLabel Player1Name = new JLabel(player.getName() + " (X) : ");
-				Player1Name.setFont(new Font("Tahoma", Font.BOLD, 15));
-				Player1Name.setForeground(Color.WHITE);
-				Player1Name.setHorizontalAlignment(SwingConstants.CENTER);
-				Player1Name.setBounds(0, 0, 150, 50);
-				ScoreboardX.add(Player1Name);
+		// Title Bar
+		setTitle("Tic Tac Toe");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Home.class.getResource("/image/icon.png")));
 				
-					// Scoreboard X Score
-				score1 = new JLabel("0");
-				score1.setBackground(Color.WHITE);
-				score1.setFont(new Font("Tahoma", Font.BOLD, 50));
-				score1.setHorizontalAlignment(SwingConstants.CENTER);
-				score1.setBounds(0, 61, 150, 75);
-				score1.setOpaque(true);
-				ScoreboardX.add(score1);
+		// Frame Setting
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 705, 525);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(47, 79, 79));
+		contentPane.setOpaque(false);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		// Board
+		for(int i = 0; i < 9; i++)
+		{
+			btn[i] = new JButton("");
+			btn[i].setText(null);
+			btn[i].setBackground(new Color(224, 90, 71));
+			contentPane.add(btn[i]);
+			btn[i].addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					JButton buttonClicked = (JButton) arg0.getSource();
+					btnActionPerformed(buttonClicked);
+				}
+			});
+		}
+		btn[0].setBounds(92, 86, 85, 85);
+		btn[1].setBounds(204, 86, 85, 85);
+		btn[2].setBounds(318, 86, 85, 85);
+		btn[3].setBounds(92, 189, 85, 85);
+		btn[4].setBounds(204, 189, 85, 85);
+		btn[5].setBounds(318, 189, 85, 85);
+		btn[6].setBounds(92, 292, 85, 85);
+		btn[7].setBounds(204, 292, 85, 85);
+		btn[8].setBounds(318, 292, 85, 85);
 				
-				// Scoreboard Y
-					// Scoreboard Y Box
-				JPanel ScoreboardY = new JPanel();
-				ScoreboardY.setBounds(445, 237, 150, 140);
-				ScoreboardY.setBackground(new Color(224, 90, 71));
-				ScoreboardY.setOpaque(true);
-				contentPane.add(ScoreboardY);
-				ScoreboardY.setLayout(null);
+		// Play Again Button
+		JButton btnPlayAgain = new JButton("Play Again");
+		btnPlayAgain.setForeground(Color.WHITE);
+		btnPlayAgain.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnPlayAgain.setBackground(new Color(224, 90, 71));
+		btnPlayAgain.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JFrame frame = new JFrame("warning");
 				
-					// Scoreboard Y Score
-				score2 = new JLabel("0");
-				score2.setBackground(Color.WHITE);
-				score2.setHorizontalAlignment(SwingConstants.CENTER);
-				score2.setFont(new Font("Tahoma", Font.BOLD, 50));
-				score2.setBounds(0, 65, 150, 75);
-				score2.setOpaque(true);
-				ScoreboardY.add(score2);
-				
-					// Scoreboard Y Name
-				Player2Name = new JLabel(comp.getName() + " (O) : ");
-				Player2Name.setForeground(Color.WHITE);
-				Player2Name.setFont(new Font("Tahoma", Font.BOLD, 15));
-				Player2Name.setHorizontalAlignment(SwingConstants.CENTER);
-				Player2Name.setBounds(0, 0, 150, 50);
-				ScoreboardY.add(Player2Name);
-				
-				// Turn to move Info Label
-				TurnInfo.setForeground(Color.WHITE);
-				TurnInfo.setFont(new Font("Tahoma", Font.BOLD, 15));
-				TurnInfo.setHorizontalAlignment(SwingConstants.CENTER);
-				TurnInfo.setBounds(92, 31, 311, 44);
-				contentPane.add(TurnInfo);
-				
+				if(JOptionPane.showConfirmDialog(frame, "Are you sure want to restart game?", "Tic Tac Toe", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_NO_OPTION)
+				{
+					playAgain();
+				}
+			}
+		});
+		btnPlayAgain.setBounds(176, 410, 131, 34);
+		contentPane.add(btnPlayAgain);
+		
+		// Exit Button
+		JButton btnExit = new JButton("Exit");
+		btnExit.setForeground(Color.WHITE);
+		btnExit.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnExit.setBackground(new Color(224, 90, 71));
+		btnExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JFrame frame = new JFrame("exit");
 				// Background
-				TTTBackground = new JLabel("New label");
-				TTTBackground.setIcon(new ImageIcon(GamesFrame.class.getResource("/image/background.png")));
-				TTTBackground.setBounds(0, 0, 700, 500);
-				contentPane.add(TTTBackground);
+				
+				if(JOptionPane.showConfirmDialog(frame, "Are you sure want to exit game?", "Tic Tac Toe", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_NO_OPTION)
+				{
+					
+					System.exit(0);
+				}
+			}
+		});
+		btnExit.setBounds(336, 410, 131, 34);
+		contentPane.add(btnExit);
+				
+		// Scoreboard X
+			// Scoreboard X Box
+		JPanel ScoreboardX = new JPanel();
+		ScoreboardX.setForeground(Color.WHITE);
+		ScoreboardX.setBackground(new Color(224, 90, 71));
+		ScoreboardX.setOpaque(true);
+		ScoreboardX.setBounds(445, 86, 150, 137);
+		contentPane.add(ScoreboardX);
+		ScoreboardX.setLayout(null);
+
+			// Scoreboard X Name
+		JLabel Player1Name = new JLabel(player.getName() + " (X) : ");
+		Player1Name.setFont(new Font("Tahoma", Font.BOLD, 15));
+		Player1Name.setForeground(Color.WHITE);
+		Player1Name.setHorizontalAlignment(SwingConstants.CENTER);
+		Player1Name.setBounds(0, 0, 150, 50);
+		ScoreboardX.add(Player1Name);
+				
+			// Scoreboard X Score
+		score1 = new JLabel("0");
+		score1.setBackground(Color.WHITE);
+		score1.setFont(new Font("Tahoma", Font.BOLD, 50));
+		score1.setHorizontalAlignment(SwingConstants.CENTER);
+		score1.setBounds(0, 61, 150, 75);
+		score1.setOpaque(true);
+		ScoreboardX.add(score1);
+				
+		// Scoreboard Y
+			// Scoreboard Y Box
+		JPanel ScoreboardY = new JPanel();
+		ScoreboardY.setBounds(445, 237, 150, 140);
+		ScoreboardY.setBackground(new Color(224, 90, 71));
+		ScoreboardY.setOpaque(true);
+		contentPane.add(ScoreboardY);
+		ScoreboardY.setLayout(null);
+				
+			// Scoreboard Y Score
+		score2 = new JLabel("0");
+		score2.setBackground(Color.WHITE);
+		score2.setHorizontalAlignment(SwingConstants.CENTER);
+		score2.setFont(new Font("Tahoma", Font.BOLD, 50));
+		score2.setBounds(0, 65, 150, 75);
+		score2.setOpaque(true);
+		ScoreboardY.add(score2);
+				
+			// Scoreboard Y Name
+		Player2Name = new JLabel(comp.getName() + " (O) : ");
+		Player2Name.setForeground(Color.WHITE);
+		Player2Name.setFont(new Font("Tahoma", Font.BOLD, 15));
+		Player2Name.setHorizontalAlignment(SwingConstants.CENTER);
+		Player2Name.setBounds(0, 0, 150, 50);
+		ScoreboardY.add(Player2Name);
+				
+		// Turn to move Info Label
+		TurnInfo.setForeground(Color.WHITE);
+		TurnInfo.setFont(new Font("Tahoma", Font.BOLD, 15));
+		TurnInfo.setHorizontalAlignment(SwingConstants.CENTER);
+		TurnInfo.setBounds(92, 31, 311, 44);
+		contentPane.add(TurnInfo);
+				
+		// Background
+		TTTBackground = new JLabel("New label");
+		TTTBackground.setIcon(new ImageIcon(GamesFrame.class.getResource("/image/background.png")));
+		TTTBackground.setBounds(0, 0, 700, 500);
+		contentPane.add(TTTBackground);
 	}
 	
 	public void btnActionPerformed(javax.swing.JButton btn)
--- src/games/ModeFrame.java
+++ src/games/ModeFrame.java
@@ -28,67 +28,67 @@ public class ModeFrame extends JFrame {
 	 */
 	public ModeFrame() {
 		// Title Bar
-				setTitle("Tic Tac Toe");
-				setIconImage(Toolkit.getDefaultToolkit().getImage(Home.class.getResource("/image/icon.png")));
+		setTitle("Tic Tac Toe");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Home.class.getResource("/image/icon.png")));
+		
+		// Frame Setting
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 705, 525);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		// Back To Main Button
+		JButton BackToMain = new JButton("Back To Main\r\n");
+		BackToMain.setForeground(Color.WHITE);
+		BackToMain.setFont(new Font("Tahoma", Font.BOLD, 15));
+		BackToMain.setBackground(new Color(224, 90, 71));
+		BackToMain.setBounds(277, 375, 142, 40);
+		BackToMain.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+				Home frame = new Home();
+				frame.setVisible(true);
+			}
+		});
 				
-				// Frame Setting
-				setResizable(false);
-				setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				setBounds(100, 100, 705, 525);
-				contentPane = new JPanel();
-				contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
-				setContentPane(contentPane);
-				contentPane.setLayout(null);
+		// Vs Player Button
+		JButton VsPlayer = new JButton("Vs Player");
+		VsPlayer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+				PlayerFrame PlayerFrame = new PlayerFrame();
+				PlayerFrame.setVisible(true);
+			}
+		});
+		VsPlayer.setForeground(Color.WHITE);
+		VsPlayer.setFont(new Font("Dialog", Font.BOLD, 15));
+		VsPlayer.setBackground(new Color(224, 90, 71));
+		VsPlayer.setBounds(390, 304, 142, 40);
+		contentPane.add(VsPlayer);
+		contentPane.add(BackToMain);
+		
+		// Vs Computer Button
+		JButton VsComputer = new JButton("Vs Computer");
+		VsComputer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+				CompFrame CompFrame = new CompFrame();
+				CompFrame.setVisible(true);
+			}
+		});
+		VsComputer.setForeground(Color.WHITE);
+		VsComputer.setFont(new Font("Tahoma", Font.BOLD, 15));
+		VsComputer.setBackground(new Color(224, 90, 71));
+		VsComputer.setBounds(171, 304, 142, 40);
+		contentPane.add(VsComputer);
 				
-				// Back To Main Button
-				JButton BackToMain = new JButton("Back To Main\r\n");
-				BackToMain.setForeground(Color.WHITE);
-				BackToMain.setFont(new Font("Tahoma", Font.BOLD, 15));
-				BackToMain.setBackground(new Color(224, 90, 71));
-				BackToMain.setBounds(277, 375, 142, 40);
-				BackToMain.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-						dispose();
-						Home frame = new Home();
-						frame.setVisible(true);
-					}
-				});
-				
-				// Vs Player Button
-				JButton VsPlayer = new JButton("Vs Player");
-				VsPlayer.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-						dispose();
-						PlayerFrame PlayerFrame = new PlayerFrame();
-						PlayerFrame.setVisible(true);
-					}
-				});
-				VsPlayer.setForeground(Color.WHITE);
-				VsPlayer.setFont(new Font("Dialog", Font.BOLD, 15));
-				VsPlayer.setBackground(new Color(224, 90, 71));
-				VsPlayer.setBounds(390, 304, 142, 40);
-				contentPane.add(VsPlayer);
-				contentPane.add(BackToMain);
-				
-				// Vs Computer Button
-				JButton VsComputer = new JButton("Vs Computer");
-				VsComputer.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-						dispose();
-						CompFrame CompFrame = new CompFrame();
-						CompFrame.setVisible(true);
-					}
-				});
-				VsComputer.setForeground(Color.WHITE);
-				VsComputer.setFont(new Font("Tahoma", Font.BOLD, 15));
-				VsComputer.setBackground(new Color(224, 90, 71));
-				VsComputer.setBounds(171, 304, 142, 40);
-				contentPane.add(VsComputer);
-				
-				// Background
-				JLabel TTTBackground = new JLabel("New label");
-				TTTBackground.setIcon(new ImageIcon(PlayerFrame.class.getResource("/image/background.png")));
-				TTTBackground.setBounds(0, 0, 700, 500);
-				contentPane.add(TTTBackground);
+		// Background
+		JLabel TTTBackground = new JLabel("New label");
+		TTTBackground.setIcon(new ImageIcon(PlayerFrame.class.getResource("/image/background.png")));
+		TTTBackground.setBounds(0, 0, 700, 500);
+		contentPane.add(TTTBackground);
 	}
 }
